4e1a7d41bdf75090930751638c2905eb7da136c1
Tim Tyrrell <timomitchel23@gmail.com>
2017-10-04 10:55:08 -0600

    refactored

--- lib/bad_connection.rb
+++ lib/bad_connection.rb
@@ -17,36 +17,60 @@ class BadConnection
     @goodbyes = []
   end
 
+  def empty_inputs
+    @empty_input_count += 1
+    @empty_input << @input
+    puts "HELLO?"
+  end
+
+  def lowercase_question
+    @lowercase_questions_count += 1
+    @lowercase_questions << @input
+    puts "I'M HAVING A HARD TIME HEARING YOU."
+  end
+
+  def uppercase_question
+    @uppercase_questions_count += 1
+    @uppercase_questions << @input
+    puts "NO THIS IS NOT A PET STORE."
+  end
+
+  def first_goodbye
+    @goodbyes << @input
+    @goodbye_count += 1
+    puts "ANYTHING ELSE I CAN HELP WITH?"
+  end
+
+  def second_goodbye
+    @goodbyes << @input
+    puts "THANK YOU FOR CALLING!"
+    @ready_to_quit = true
+  end
+
+  def print_log
+    puts "All input:\n #{@all_input}\n"
+    puts "HELLO?\n #{@empty_input}\n output #{@empty_input_count} times"
+    puts "I'M HAVING A HARD TIME HEARIGN YOU!\n #{@lowercase_questions}\n output #{@lowercase_questions_count} times"
+    puts "NO THIS IS NOT A PET STORE. \n #{@uppercase_questions}\n output #{@uppercase_questions_count} times"
+    puts "GOODBYE!\n #{@goodbyes}\n output #{@goodbye_count} times"
+  end
+
   def start
     until @ready_to_quit == true
-      input = gets.chomp
-      @all_input << input
-      if input == ""
-        @empty_input_count += 1
-        @empty_input << input
-        puts "HELLO?"
-      elsif input =~ /[a-z]/ && input[-1] == "?"
-        @lowercase_questions_count += 1
-        @lowercase_questions << input
-        puts "I'M HAVING A HARD TIME HEARING YOU."
-      elsif input == input.upcase && input.end_with?('?')
-        @uppercase_questions_count += 1
-        @uppercase_questions << input
-        puts "NO THIS IS NOT A PET STORE."
-      elsif input == "GOODBYE!" && @goodbye_count == 0
-        @goodbyes << input
-        @goodbye_count += 1
-        puts "ANYTHING ELSE I CAN HELP WITH?"
-      elsif input == "GOODBYE!" && @goodbye_count == 1
-        @goodbyes << input
-        puts "THANK YOU FOR CALLING!"
-        @ready_to_quit = true
-      elsif input == "print log"
-        puts "All input:\n #{@all_input}\n"
-        puts "HELLO?\n #{@empty_input}\n output #{@empty_input_count} times"
-        puts "I'M HAVING A HARD TIME HEARIGN YOU!\n #{@lowercase_questions}\n output #{@lowercase_questions_count} times"
-        puts "NO THIS IS NOT A PET STORE. \n #{@uppercase_questions}\n output #{@uppercase_questions_count} times"
-        puts "GOODBYE!\n #{@goodbyes}\n output #{@goodbye_count} times"
+      @input = gets.chomp
+      @all_input << @input
+      if @input == ""
+        empty_inputs
+      elsif @input =~ /[a-z]/ && @input[-1] == "?"
+        lowercase_question
+      elsif @input == @input.upcase && @input.end_with?('?')
+        uppercase_question
+      elsif @input == "GOODBYE!" && @goodbye_count == 0
+        first_goodbye
+      elsif @input == "GOODBYE!" && @goodbye_count == 1
+        second_goodbye
+      elsif @input == "print log"
+        print_log
       else
         @ready_to_quit = true
       end
--- test/bad_connection_test.rb
+++ test/bad_connection_test.rb
@@ -2,6 +2,7 @@ require "minitest/autorun"
 require "minitest/pride"
 require "./lib/bad_connection"
 
+#When running tests type "a" and enter when prompted for user input to see tests run
 class BadConnectionTest < Minitest::Test
 
   def test_that_class_exist_and_initialize_methods_setup_correct
@@ -21,13 +22,50 @@ class BadConnectionTest < Minitest::Test
     assert_equal 0, connection.goodbye_count
   end
 
-  def test_start_method
+  def test_empty_inputs
     connection = BadConnection.new
-    connection.start
-    #type "a" without a question mark
+    connection.empty_inputs
 
-    assert_equal true, connection.ready_to_quit
+    assert_equal 1, connection.empty_input_count
+    assert_equal [nil], connection.empty_input
   end
 
+  def test_lowercase_question
+    connection = BadConnection.new
+
+    assert_equal 0, connection.lowercase_questions_count
+
+    connection.lowercase_question
+
+    assert_equal 1, connection.lowercase_questions_count
+    assert_equal [nil], connection.lowercase_questions
+  end
+
+  def test_uppercase_question
+    connection = BadConnection.new
+
+    assert_equal 0, connection.uppercase_questions_count
+
+    connection.uppercase_question
+
+    assert_equal 1, connection.uppercase_questions_count
+    assert_equal [nil], connection.uppercase_questions
+  end
+
+  def test_first_goodbye
+    connection = BadConnection.new
+    connection.first_goodbye
+
+    assert_equal 1, connection.goodbye_count
+    assert_equal [nil], connection.goodbyes
+  end
+
+  def test_second_goodbye
+    connection = BadConnection.new
+    connection.second_goodbye
+
+    assert_equal [nil], connection.goodbyes
+    assert_equal true, connection.ready_to_quit
+  end
 
 end
